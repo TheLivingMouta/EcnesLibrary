3def11ad96508d8e9be80b2a0023bd09fd87296b
Zach Affron <carolanbrian77@yahoo.ie>
2024-08-28 19:34:04 +0100

    Validation for Book creation

--- EcnesLibrary/Models/Books.cs
+++ EcnesLibrary/Models/Books.cs
@@ -1,17 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EcnesLibrary.Models;
 
-    public class Books
-    {
+public class Books
+{
 
     [Key]
     public int BookId { get; set; }
+
+    [StringLength(50, MinimumLength = 3)]
+    [Required]
     public string Title { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string Author { get; set; }
     public string Genre { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Required]
+    [Column(TypeName = "Decimal(18,2)")]
     public decimal Price { get; set; }
 
 
-    }
+}
 
